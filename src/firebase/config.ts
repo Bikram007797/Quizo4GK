66c53956ac1e5231f9e29d5c203b761d444d5bf6
Bikram Mondal <bikarammondal74@gmail.com>
2025-12-04 18:59:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8178033783-96aea",
+  "appId": "1:1042317564782:web:ae283edddb81f200f43f7f",
+  "apiKey": "AIzaSyBCbox-Hw5lB9cPuCMF3WGW73v5aZJw2Ag",
+  "authDomain": "studio-8178033783-96aea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1042317564782"
+};
